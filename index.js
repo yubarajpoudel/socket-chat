adafb8f76c1b9ee8c6fd88e637c7289f83f4b462
yubaraj poudel <yubarajpoudel708@gmail.com>
2019-11-01 14:54:36 +0545

    port changed to 80

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
